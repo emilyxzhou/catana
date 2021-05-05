a45bf9051835113b952a9802a9e297865d395141
liu-jocel <liujocel@usc.edu>
2021-05-04 21:40:17 -0400

    Update snapshot.sh

--- scripts/snapshot.sh
+++ scripts/snapshot.sh
@@ -5,5 +5,5 @@ HOST=$2
 PORT=$3
 fswebcam --no-banner /home/pi/Documents/ee250-final/$FILE_NAME.jpg
 
-python3 img_pub.py $FILE_NAME.jpg $HOST $PORT
+python3 scripts/img_pub.py $FILE_NAME.jpg $HOST $PORT
 rm $FILE_NAME.jpg
